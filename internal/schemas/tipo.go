a423f8b405ae702c3a1e19afb27cb042b14e8f15
Lucio Pintanel <lm.pintanel@gmail.com>
2023-08-30 11:13:50 -0300

    - Atualizado arquivos de módulos. - Criado o primeiro schema(tabela).

--- internal/schemas/tipo.go
+++ internal/schemas/tipo.go
@@ -1,1 +1,20 @@
 package schemas
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Tipo struct {
+	gorm.Model
+	Descricao string
+}
+
+type TipoResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeleteAt  time.Time `json:"deleteAt,omitempty"`
+	Descricao string    `json:"descricao"`
+}
